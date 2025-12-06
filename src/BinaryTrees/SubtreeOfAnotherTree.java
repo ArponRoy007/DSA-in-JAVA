40f816f4451a784f4d565c44e84fec26d25e44a6
Arpon Roy <royarpon007@gmail.com>
2025-12-06 14:31:56 +0530

    added binary tree code

--- src/BinaryTrees/SubtreeOfAnotherTree.java
+++ src/BinaryTrees/SubtreeOfAnotherTree.java
@@ -1,5 +1,6 @@
 package BinaryTrees;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -104,6 +105,64 @@ public class SubtreeOfAnotherTree {
     }
 
 
+    //K-TH LEVEL
+    public static void KLevel(Node root, int level, int k){
+        if (root==null){
+            return;
+        }
+
+        if (level==k){
+            System.out.println(root.data+" ");
+            return;
+        }
+
+        KLevel(root.left, level+1, k);
+        KLevel(root.right, level+1, k);
+    }
+
+
+    //LOWEST COMMON ANCESTORS
+
+    public static boolean getPath(Node root, int n, ArrayList<Node> path){
+        if (root==null){
+            return false;
+        }
+        path.add(root);
+
+        if (root.data==n){
+            return  true;
+        }
+
+        boolean foundLeft=getPath(root.left, n, path);
+        boolean foundRight=getPath(root.right, n, path);
+
+        if (foundLeft||foundRight){
+            return true;
+        }
+        path.remove(path.size()-1);
+        return false;
+    }
+
+    public static Node lca(Node root, int n1, int n2){
+        ArrayList<Node> path1=new ArrayList<>();
+        ArrayList<Node> path2=new ArrayList<>();
+
+        getPath(root, n1, path1);
+        getPath(root, n2, path2);
+
+        //lca
+        int i=0;
+        for (; i<path1.size() && i<path2.size(); i++){
+            if (path1.get(i)!=path2.get(i)){
+                break;
+            }
+        }
+
+        Node lca=path1.get(i-1);
+        return lca;
+    }
+
+
     public static void main(String[] args) {
         // Main Tree (Root) তৈরি
         Node root = new Node(1);
@@ -127,5 +186,10 @@ public class SubtreeOfAnotherTree {
         //System.out.println(isSubtree(root,subRoot));
 
         topView(root);
+
+        int k=3;
+        KLevel(root,1, k);
+
+        System.out.println(lca(root, 4,5).data);
     }
 }
